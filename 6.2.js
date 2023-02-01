28985d4cd2d4cf4740d692b4441177c5117ff5c0
Zhu Wentao <zhu.wentao@u.yale-nus.edu.sg>
2023-02-01 19:12:20 +0200

    6.2.js

---
+++ 6.2.js
@@ -0,0 +1,15 @@
+// use deno fmt 6.2.js to format the code
+
+const name = "My name!";
+const age = 180;
+
+console.log(`My name is ${name} and I am ${age} years old`);
+
+// use deno lint --unstable 6.2.js to check the code
+
+const first = "test";
+const second = "test";
+
+if (first === second) {
+  console.log("Hello world!");
+}
